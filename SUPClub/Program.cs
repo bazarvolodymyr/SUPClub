bd4c61c76b70a66d37e09c8a043d03f8888edf9e
smile <smile@DESKTOP-9LLAKHH>
2025-03-28 18:48:53 +0200

    add website template

--- SUPClub/Program.cs
+++ SUPClub/Program.cs
@@ -1,6 +1,13 @@
 var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddControllersWithViews();
 var app = builder.Build();
+if (app.Environment.IsDevelopment())
+{
+    app.UseDeveloperExceptionPage();
+}
 
-app.MapGet("/", () => "Hello World!");
+app.UseStaticFiles();
+
+app.MapControllerRoute("default", "{controller=Home}/{action=Index}/{id?}");
 
 app.Run();
